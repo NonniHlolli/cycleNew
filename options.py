457b3fe5a37e3c7c8e0f7cea6a8bbe9e02dae5c2
NonniHlolli <jhf1195@gmail.com>
2021-05-20 17:29:20 +0000

    blu

--- options.py
+++ options.py
@@ -23,3 +23,25 @@ class Options():
         self.cuda = cuda
         # no. of cpu
         self.n_cpu = n_cpu
+
+
+class TestOptions():
+
+    def __init__(batchSize = 1, dataroot = './apple2orange', size = 256, input_nc = 3, output_nc = 3, cuda = False, n_cpu = 8, generator_A2B = 'output/netG_A2B.pth',  generator_B2A = 'output/netG_B2A.pth'):
+        self.batchSize = batchSize
+        # size of data
+        self.size = size
+        # input channels
+        self.input_nc = input_nc
+        # output channels
+        self.output_nc = output_nc
+        # use GPU
+        self.cuda = cuda
+        # no. of cpu
+        self.n_cpu = n_cpu
+        # directory of data
+        self.dataroot = dataroot
+        # directory of generator A to B
+        self.generator_A2B = generator_A2B
+        # directory of generator A to B
+        self. generator_B2A = generator_B2A
